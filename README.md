beda2bbe446103d9b38e23bdb5828f861edd67d4
aidan <aidan.roche@ynov.com>
2026-09-11 15:55:55 +0200

    feat: implement beverage dispenser simulator and add project documentation

---
+++ README.md
@@ -0,0 +1,12 @@
+# Évaluation Go
+
+Nom : Roche
+Prénom : Aidan
+
+## Exercices
+
+### Exercice 1
+Programme qui simule un distributeur de boissons. L'utilisateur choisit une boisson dans le menu, entre son argent, et le programme calcule la monnaie ou redemande s'il manque des sous. La boucle permet de recommencer jusqu'à taper 0 pour quitter.
+
+### Exercice 2
+Programme qui demande un nombre de notes, vérifie qu'elles sont bien entre 0 et 20, puis calcule la moyenne, la note max et la note min. Il affiche si l'élève est admis et inclut le bonus avec les appréciations et le nombre de notes au-dessus de la moyenne.
--- exercice1/main.go
+++ exercice1/main.go
@@ -1,1 +1,98 @@
 package main
+
+import "fmt"
+
+const (
+	Reset = "\033[0m"
+	Gras  = "\033[1m"
+	Cyan  = "\033[36m"
+	Rouge = "\033[31m"
+	Vert  = "\033[32m"
+) //codes couleurs golang
+
+func main() {
+	for {
+		afficherMenu()
+
+		var choix int
+		fmt.Print("Votre choix : ")
+		fmt.Scan(&choix) //récupère le choix
+
+		if choix == 0 {
+			fmt.Println("Aurevoir !")
+			break
+		} // Option quitter du menu
+		if choix < 1 || choix > 4 {
+			fmt.Println(Rouge + "Choix invalide" + Reset)
+			fmt.Println()
+			continue
+		} // Check si le choix est correct (compris entre 0 et 4)
+		fmt.Println() //saut de ligne pour la lisibilité
+
+		afficherBoisson(choix) // Affiche la boisson choisie
+
+		prix := obtenirPrix(choix) // Récupère le prix de la boisson
+		fmt.Printf("Prix : %d €", prix)
+		fmt.Println()
+		fmt.Println(Vert + "-----------------------" + Reset)
+
+		var montant int
+		fmt.Println()
+		fmt.Print("Montant donné :")
+		fmt.Scan(&montant)
+
+		if montant < prix {
+			manque := prix - montant
+			fmt.Printf(Rouge+Gras+"Il manque %d €\n"+Reset, manque)
+			fmt.Println()
+			continue
+		} // Check si le montant est suffisant
+
+		rendu := montant - prix // Calcul du rendu
+		fmt.Println()
+		fmt.Println(Vert + "Merci pour votre achat !" + Reset)
+		fmt.Printf("Votre monnaie : %d €\n", rendu)
+		fmt.Println()
+	}
+}
+
+func afficherMenu() {
+	fmt.Println(Cyan + Gras + "=== DISTRIBUTEUR ===" + Reset)
+	fmt.Println()
+	fmt.Println("1 - Eau       : 1 €")
+	fmt.Println("2 - Soda      : 2 €")
+	fmt.Println("3 - Café      : 2 €")
+	fmt.Println("4 - Chocolat  : 3 €")
+	fmt.Println("0 - Quitter")
+	fmt.Println(Vert + "-----------------------" + Reset)
+} //Afficher le menu
+
+func obtenirPrix(choix int) int {
+	switch choix {
+	case 1:
+		return 1
+	case 2:
+		return 2
+	case 3:
+		return 2
+	case 4:
+		return 3
+	default:
+		return 0
+	}
+} //swich , similiraire a if... else if...
+
+func afficherBoisson(choix int) {
+	switch choix {
+	case 1:
+		fmt.Println("Vous avez choisi : Eau")
+	case 2:
+		fmt.Println("Vous avez choisi : Soda")
+	case 3:
+		fmt.Println("Vous avez choisi : Café")
+	case 4:
+		fmt.Println("Vous avez choisi : Chocolat")
+	default:
+		fmt.Println(Rouge + "Choix invalide" + Reset)
+	}
+} //encore une fois, un switch
